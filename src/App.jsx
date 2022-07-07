9b7d8bf62a6ca1d879b74292eb6d79e0d65f339b
Mainmar1 <juan_bj95@hotmail.com>
2022-07-07 10:23:20 -0300

    new

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import image1 from "./";
+import "./App.css";
+import Navbar from "./views/components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <img src={image1} /> */}
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/views/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./_navbar.css";
+
+function Navbar() {
+  return (
+    <div className="navbar navbar-inverse navbar-fixed-top">
+      <div>
+        <p>Logo</p>
+      </div>
+      <ul className="nav navbar-nav">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Products</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+        <li>
+          <a href="#">Account</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
